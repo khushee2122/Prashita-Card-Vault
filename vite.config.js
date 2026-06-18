9b628011b3d985f67a523e00812556e75b730cc1
Khushee Patel <kbpatel2122@gmail.com>
2026-06-18 18:06:02 +0530

    fix PWA offline caching - cache all app assets

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,29 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'gstatic-fonts-cache',
+              expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 }
+            }
+          }
+        ],
+        navigateFallback: 'index.html',
+        navigateFallbackDenylist: [/^\/api/]
+      },
       manifest: {
         name: 'CardVault',
         short_name: 'CardVault',
@@ -16,6 +39,7 @@ export default defineConfig({
         background_color: '#F8F9FC',
         display: 'standalone',
         orientation: 'portrait',
+        start_url: '/',
         icons: [
           { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
